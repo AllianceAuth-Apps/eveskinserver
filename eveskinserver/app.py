3dd817e646806cb325e8735e2a3c10f0ffec8968
ErikKalkoken <erik.kalkoken@gmail.com>
2020-12-25 23:07:07 +0100

    Add logging

--- eveskinserver/app.py
+++ eveskinserver/app.py
@@ -1,24 +1,30 @@
 import csv
-import tempfile
+import logging
 import os
+import tempfile
 
 from flask import Flask, send_file, request, render_template, make_response, abort
-
 from PIL import Image
+
+logger = logging.getLogger(__name__)
+c_handler = logging.StreamHandler()
+c_handler.setLevel(logging.INFO)
+c_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+c_handler.setFormatter(c_format)
+logger.addHandler(c_handler)
 
 app = Flask(__name__)
 
 
 current_path = os.path.dirname(os.path.realpath(__file__))
-
 generated_icons_path = tempfile.mkdtemp()
-print(f"Storing generated icons in: {generated_icons_path}")
+logger.debug("Storing generated icons in: %s", generated_icons_path)
 
 
 def load_type_2_icon() -> dict:
     """returns generated mapping from type ID to icon ID"""
 
-    print("Building type to icon ID mappings...")
+    logger.info("Building type to icon ID mappings...")
     with open(f"{current_path}/ccp_data/sde/skinLicense.csv", mode="r") as csv_file:
         skin_licenses = list(csv.reader(csv_file, delimiter=","))
 
@@ -48,6 +54,7 @@ def api(type_id):
     try:
         size = int(request.args.get("size"))
     except TypeError:
+        logger.warning("Invalid size provided. Falling back to 64 as default.")
         size = None
 
     if not size or size < 32 or size > 1024 or (size & (size - 1) != 0):
@@ -77,10 +84,14 @@ def icon_sized_filename(icon_name: str, size: int) -> str:
 
 
 def generate_sized_icon(icon_name: str, size: int) -> str:
+    logger.info("Generating icon for %s of size %s", icon_name, size)
     try:
         with Image.open(icon_base_filename(icon_name)) as img:
             img.load()
     except FileNotFoundError:
+        logger.warning(
+            "Could not find icon file for '%s'. Using default instead.", icon_name
+        )
         with Image.open(icon_base_filename("default")) as img:
             img.load()
 
