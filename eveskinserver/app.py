9b3db040e52d8f53169c21af4d97cf7cf81a61cd
ErikKalkoken <erik.kalkoken@gmail.com>
2020-12-26 02:14:11 +0100

    Fix log output for unknown icons

--- eveskinserver/app.py
+++ eveskinserver/app.py
@@ -19,21 +19,21 @@ DEFAULT_SKIN_ICON = "102"  # SKIN icon to use if icon file is missing
 
 app = Flask(__name__)
 
-current_path = os.path.dirname(os.path.realpath(__file__))
-generated_icons_path = tempfile.mkdtemp()
-app.logger.info("Storing generated icons in: %s", generated_icons_path)
+current_folder = os.path.dirname(os.path.realpath(__file__))
+generated_icons_folder = tempfile.mkdtemp()
+app.logger.info("Storing generated icons in: %s", generated_icons_folder)
 
 
 def load_type_2_icon() -> dict:
     """returns generated mapping from type ID to icon ID"""
 
     app.logger.info("Building type to icon ID mappings...")
-    with open(f"{current_path}/ccp_data/sde/skinLicense.csv", mode="r") as csv_file:
+    with open(f"{current_folder}/ccp_data/sde/skinLicense.csv", mode="r") as csv_file:
         skin_licenses = list(csv.reader(csv_file, delimiter=","))
 
     type_2_skin = {row[0]: row[2] for row in skin_licenses}
 
-    with open(f"{current_path}/ccp_data/sde/skins.csv", mode="r") as csv_file:
+    with open(f"{current_folder}/ccp_data/sde/skins.csv", mode="r") as csv_file:
         skins = list(csv.reader(csv_file, delimiter=","))
 
     skin_2_icon = {row[0]: row[2] for row in skins}
@@ -47,7 +47,7 @@ type_to_icon_mapping = load_type_2_icon()
 
 @app.route("/favicon.ico")
 def favicon():
-    return send_file(f"{current_path}/static/icon.png", mimetype="image/png")
+    return send_file(f"{current_folder}/static/icon.png", mimetype="image/png")
 
 
 @app.route("/", methods=["GET"])
@@ -73,43 +73,44 @@ def api(type_id):
         abort(404)
 
     icon_name = str(icon_id)
-    output_filename = icon_sized_filename(icon_name, size)
-    if not os.path.isfile(output_filename):
-        output_filename = generate_sized_icon(icon_name, size)
+    output_filepath = icon_sized_filepath(icon_name, size)
+    if not os.path.isfile(output_filepath):
+        output_filepath = generate_sized_icon(icon_name, size)
 
-    response = make_response(send_file(output_filename, mimetype="image/png"))
-    _, filename = os.path.split(output_filename)
+    response = make_response(send_file(output_filepath, mimetype="image/png"))
+    _, filename = os.path.split(output_filepath)
     response.headers["x-suggested-filename"] = filename
     return response
 
 
-def icon_base_filename(icon_name: str) -> str:
-    return f"{current_path}/ccp_data/icons/{icon_name}.png"
+def icon_base_filepath(icon_name: str) -> str:
+    return f"{current_folder}/ccp_data/icons/{icon_name}.png"
 
 
-def icon_sized_filename(icon_name: str, size: int) -> str:
-    return f"{generated_icons_path}/{icon_name}_{size}.png"
+def icon_sized_filepath(icon_name: str, size: int) -> str:
+    return f"{generated_icons_folder}/{icon_name}_{size}.png"
 
 
 def generate_sized_icon(icon_name: str, size: int) -> str:
+    """generated a sized icon and stores it. Returns the full path to that file"""
     app.logger.info("Generating icon for %s of size %s", icon_name, size)
     try:
-        with Image.open(icon_base_filename(icon_name)) as img:
+        with Image.open(icon_base_filepath(icon_name)) as img:
             img.load()
     except FileNotFoundError:
-        icon_name = DEFAULT_SKIN_ICON
         app.logger.warning(
             "Could not find icon file for '%s'. Using default instead.", icon_name
         )
-        with Image.open(icon_base_filename(icon_name)) as img:
+        icon_name = DEFAULT_SKIN_ICON
+        with Image.open(icon_base_filepath(icon_name)) as img:
             img.load()
 
     w_percent = size / float(img.size[0])
     h_size = int((float(img.size[1]) * float(w_percent)))
     img = img.resize((size, h_size), Image.ANTIALIAS)
-    output_filename = icon_sized_filename(icon_name, size)
-    img.save(output_filename)
-    return output_filename
+    output_filepath = icon_sized_filepath(icon_name, size)
+    img.save(output_filepath)
+    return output_filepath
 
 
 if __name__ == "__main__":
